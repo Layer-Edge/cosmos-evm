e282a39adc358dada284afbe2d76dcf181b32329
Kunjal Tandel <kunjal.tandel24@gmail.com>
2025-05-17 13:47:44 +0530

    fix: evm native token to map with secondary token of cosmos

--- cmd/evmd/config/config.go
+++ cmd/evmd/config/config.go
@@ -18,8 +18,8 @@ var ChainsCoinInfo = map[uint64]evmtypes.EvmCoinInfo{
 		Decimals:      evmtypes.EighteenDecimals,
 	},
 	CosmosChainID: {
-		Denom:         "sedgen",
-		ExtendedDenom: "sedgen",
+		Denom:         "tedgen",
+		ExtendedDenom: "tedgen",
 		DisplayDenom:  "edgen",
 		Decimals:      evmtypes.EighteenDecimals,
 	},
@@ -41,9 +41,9 @@ const (
 	// Bech32PrefixConsPub defines the Bech32 prefix of a consensus node public key.
 	Bech32PrefixConsPub = Bech32Prefix + sdk.PrefixValidator + sdk.PrefixConsensus + sdk.PrefixPublic
 	// DisplayDenom defines the denomination displayed to users in client applications.
-	DisplayDenom = "atom"
+	DisplayDenom = "edgen"
 	// BaseDenom defines to the default denomination used in the Cosmos EVM example chain.
-	BaseDenom = "aatom"
+	BaseDenom = "tedgen"
 	// BaseDenomUnit defines the precision of the base denomination.
 	BaseDenomUnit = 18
 	// EVMChainID defines the EIP-155 replay-protection chain id for the current ethereum chain config.
